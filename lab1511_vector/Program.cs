27e741a3ebc65530a7f84307c4e2b9fc8a00422f
hallgató <hallgat@LABOR115-02>
2017-11-15 11:18:01 +0100

    Add project files.

---
+++ lab1511_vector/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1511_vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vector in (x,y) format: ");
+            string s = Console.ReadLine();
+            Vector v1 = Vector.Parse(s);
+            Console.WriteLine(v1.print());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab1511_vector/Vector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1511_vector
+{
+    class Vector
+    {
+        double x;
+        public double X { get { return x; } set { x = value; } }
+        double y;
+        public double Y { get { return y; } set { y = value; } }
+
+        public Vector(double x, double y)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public static Vector Parse(string str)
+        {
+            int v = str.IndexOf(",");
+            int x = int.Parse(str.Substring(0, v));
+            int y = int.Parse(str.Substring(v + 1));
+            return new Vector(x, y);
+        }
+        public string print()
+        {
+            return "x= (" + x + ", " + y + ")";
+        }
+    }
+}
